9fae91266122983c1461c5b9e716089cf3b223bd
andy goryachev <andy@goryachev.com>
2020-10-23 15:22:15 -0700

    .

---
+++ src/goryachev/fxtexteditor/AStyledCellTextLine.java
@@ -0,0 +1,74 @@
+// Copyright © 2020 Andy Goryachev <andy@goryachev.com>
+package goryachev.fxtexteditor;
+import javafx.scene.paint.Color;
+
+
+/**
+ * An ITextLine implementation based on CellStyleArray.
+ */
+public abstract class AStyledCellTextLine
+	implements ITextLine
+{
+	/** creates cell style array based on the plain text */
+	protected abstract CellStyleArray createCellStyleArray(String text);
+	
+	//
+	
+	protected final int line;
+	protected final String plainText;
+	private CellStyleArray styles;
+	
+	
+	public AStyledCellTextLine(int line, String text)
+	{
+		this.line = line;
+		this.plainText = text;
+	}
+	
+	
+	public int getTextLength()
+	{
+		return plainText.length();
+	}
+
+
+	public String getPlainText()
+	{
+		return plainText;
+	}
+
+
+	public int getModelIndex()
+	{
+		return line;
+	}
+
+
+	public int getLineNumber()
+	{
+		return line;
+	}
+
+
+	public CellStyle getCellStyle(int charOffset)
+	{
+		return styles().get(charOffset);
+	}
+	
+	
+	public Color getLineColor()
+	{
+		return styles().getLineColor();
+	}
+	
+	
+	protected CellStyleArray styles()
+	{
+		if(styles == null)
+		{
+			styles = createCellStyleArray(plainText);
+		}
+
+		return styles;
+	}
+}
