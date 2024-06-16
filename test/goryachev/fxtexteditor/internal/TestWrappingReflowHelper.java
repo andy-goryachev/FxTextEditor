25281ddc4e0833161ada70e4e8e695088003b36b
andy goryachev <andy@goryachev.com>
2024-06-16 15:33:21 -0700

    2024

--- test/goryachev/fxtexteditor/internal/TestWrappingReflowHelper.java
+++ test/goryachev/fxtexteditor/internal/TestWrappingReflowHelper.java
@@ -1,4 +1,4 @@
-// Copyright © 2019-2023 Andy Goryachev <andy@goryachev.com>
+// Copyright © 2019-2024 Andy Goryachev <andy@goryachev.com>
 package goryachev.fxtexteditor.internal;
 import goryachev.common.test.TF;
 import goryachev.common.test.Test;
